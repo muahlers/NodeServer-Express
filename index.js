eda579703122fafe3f2661874d3a8636c31d16a2
muahlers <muahlers@gmail.com>
2020-11-10 18:04:35 -0300

    First GetEnd Points on Server.

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express');   // Requiro Paquetes Express en node_module
 const app = express();                // Abro una instancia Express y la llamo app!
 const port = 3000;                  // Defino un Puerto a Usar por el Server.
 
+app.get('/', (request, response) => {
+  response.send("Hello World");
+});
+
+app.get('/test', (request, response) => {
+  response.send("Test");
+});
+
 app.listen(port, () => {
   console.log(`Server is Running in Port: ${port}`);
-})
+});
