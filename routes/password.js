6effcc8ad939f50dc1bffedceeb68e9494be9694
muahlers <muahlers@gmail.com>
2020-11-12 16:48:59 -0300

    First Step to Email Sender

--- routes/password.js
+++ routes/password.js
@@ -1,4 +1,30 @@
 const express = require('express');
+const nodemailer = require('nodemailer');
+const hbs = require('nodemailer-express-handlebars');
+const path = require('path');
+
+// Configuro Email Sender
+const email = process.env.EMAIL_ACCOUNT
+const password = process.env.EMAIL_PASSWORD
+
+const stmpTransport = nodemailer.createTransport({
+  service: process.env.EMAIL_PROVAIDER,
+  auth: {
+    user: email,
+    pass: password
+  }
+});
+
+const handlebarsOptions = {
+  viewEngine: {
+    extName: '.hbs',
+    partialsDir: './templates/',
+    layoutsDir: './templates'
+  },
+  viewPath: path.resolve('./templates/'),
+  extName: '.html'
+};
+
 // Creo una instancia de express para manejar rutas llamada Router.
 const router = express.Router();
 
