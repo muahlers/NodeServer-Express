e7117db36a15f729cbf5e01c549f2b6ca1aa0b3a
muahlers <muahlers@gmail.com>
2020-11-12 18:40:25 -0300

    Fin del Día 3

--- routes/password.js
+++ routes/password.js
@@ -18,6 +18,7 @@ const stmpTransport = nodemailer.createTransport({
 const handlebarsOptions = {
   viewEngine: {
     extName: '.hbs',
+    defaultLayout: null,
     partialsDir: './templates/',
     layoutsDir: './templates'
   },
@@ -30,21 +31,55 @@ stmpTransport.use('compile', hbs(handlebarsOptions));
 // Creo una instancia de express para manejar rutas llamada Router.
 const router = express.Router();
 
-router.post('/forget-password', (request, response) => {
+router.post('/forget-password', async (request, response, done) => {
   if(!request.body || !request.body.email) {
     response.status(400).json({ message: "invalid email", status: "400"});
   } else {
-        const { email } = request.body; // const email = response.body.email.
-        response.status(200).json({ message: `forgot password requested for email ${email}`, status: "200"});
-  }
+          try{
+            const userEmail = request.body.email;
+            // Send User a Email to reset password
+            const emailOptions = {
+              to: userEmail,
+              from: email,
+              template: 'forgot-password',
+              subject: 'Game Reset Password',
+              // Aqui pongo las variables que van dentro del email
+              context: {
+                name: 'joe',
+                url: `http://localhost:${process.env.PORT || 3000}`
+              }
+            };
+            await stmpTransport.sendMail(emailOptions);
+            response.status(200).json({ message: 'An email has been sent to your email address, Password reset link is only valid for 10 min', status: "200"});
+          } catch (error) {
+                return done(error);
+          }
+    }
 });
 
-router.post('/reset-password', (request, response) => {
+router.post('/reset-password', async (request, response, done) => {
   if(!request.body || !request.body.email) {
     response.status(400).json({ message: "invalid email", status: "400"});
   } else {
-        const { email } = request.body; // const email = response.body.email.
-        response.status(200).json({ message: `password reset requested for email ${email}`, status: "200"});
+      try {
+          const userEmail = request.body.email;
+
+          // Send User a Email telling password updates
+          const emailOptions = {
+            to: userEmail,
+            from: email,
+            template: 'reset-password',
+            subject: 'Game Updated Password',
+            // Aqui pongo las variables que van dentro del email
+            context: {
+              name: 'joe'
+            }
+          };
+          await stmpTransport.sendMail(emailOptions);
+          response.status(200).json({ message: 'password updated', status: "200"});
+        } catch (error) {
+            return done(error);
+        }
   }
 });
 
